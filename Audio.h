2a63896a70fc29d656976cb3632a2f538c498b0e
PaulStoffregen <paul@pjrc.com>
2014-01-19 05:10:19 -0800

    Add AudioAnalyzeToneDetect

--- Audio.h
+++ Audio.h
@@ -387,13 +387,57 @@ private:
 
 
 
+
+class AudioAnalyzeToneDetect : public AudioStream
+{
+public:
+	AudioAnalyzeToneDetect(void)
+	  : AudioStream(1, inputQueueArray), thresh(6554), enabled(false) { }
+	void frequency(float freq, uint16_t cycles=10) {
+		set_params((int32_t)(cos((double)freq
+		  * (2.0 * 3.14159265358979323846 / AUDIO_SAMPLE_RATE_EXACT))
+		  * (double)2147483647.999), cycles,
+		  (float)AUDIO_SAMPLE_RATE_EXACT / freq * (float)cycles + 0.5f);
+	}
+	void set_params(int32_t coef, uint16_t cycles, uint16_t len);
+	bool available(void) {
+		__disable_irq();
+		bool flag = new_output;
+		if (flag) new_output = false;
+		__enable_irq();
+		return flag;
+	}
+	float read(void);
+	void threshold(float level) {
+		if (level < 0.01f) thresh = 655;
+		else if (level > 0.99f) thresh = 64881;
+		else thresh = level * 65536.0f + 0.5f;
+	}
+	operator bool();  // true if at or above threshold, false if below
+	virtual void update(void);
+private:
+	int32_t coefficient;	// Goertzel algorithm coefficient
+	int32_t s1, s2;		// Goertzel algorithm state
+	int32_t out1, out2;	// Goertzel algorithm state output
+	uint16_t length;	// number of samples to analyze
+	uint16_t count;		// how many left to analyze
+	uint16_t ncycles;	// number of waveform cycles to seek
+	uint16_t thresh;	// threshold, 655 to 64881 (1% to 99%)
+	bool enabled;
+	volatile bool new_output;
+	audio_block_t *inputQueueArray[1];
+};
+
+
+
+
+
+
+
 // TODO: more audio processing objects....
-//  N-channel mixer, adjustable gain on each channel
 //  sine wave with frequency modulation (phase)
-//  non-sine oscillators, ramp, triangle, square/pulse, etc
+//  waveforms with bandwidth limited tables for synth
 //  envelope: attack-decay-sustain-release, maybe other more complex?
-//  filters, low pass, high pass, bandpass, notch
-//  frequency analysis - FFT, single frequency (eg, filter for DTMF)
 //  MP3 decoding - it is possible with optimized code?
 //  other decompression, ADPCM, Vorbis, Speex, etc?
 
