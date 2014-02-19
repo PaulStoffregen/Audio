a8fd397ba114d04e8e5ae907dff349d4f5185d02
PaulStoffregen <paul@pjrc.com>
2014-02-19 11:10:06 -0800

    Move DSP instruction stuff to its own header file

--- Audio.cpp
+++ Audio.cpp
@@ -1,6 +1,6 @@
 #include "Audio.h"
 #include "arm_math.h"
-
+#include "utility/dspinst.h"
 
 
 
@@ -46,15 +46,6 @@ static void copy_to_fft_buffer(void *destination, const void *source)
 		*dst++ = *src++;  // real
 		*dst++ = 0;       // imaginary
 	}
-}
-
-// computes limit((val >> rshift), 2**bits)
-static inline int32_t signed_saturate_rshift(int32_t val, int bits, int rshift) __attribute__((always_inline));
-static inline int32_t signed_saturate_rshift(int32_t val, int bits, int rshift)
-{
-	int32_t out;
-	asm volatile("ssat %0, %1, %2, asr %3" : "=r" (out) : "I" (bits), "r" (val), "I" (rshift));
-	return out;
 }
 
 static void apply_window_to_fft_buffer(void *buffer, const void *window)
@@ -1149,114 +1140,7 @@ void AudioPlayMemory::update(void)
 
 
 
-
-
-
-
-
-
-
 /******************************************************************/
-
-
-
-
-// computes ((a[31:0] * b[15:0]) >> 16)
-static inline int32_t signed_multiply_32x16b(int32_t a, uint32_t b) __attribute__((always_inline));
-static inline int32_t signed_multiply_32x16b(int32_t a, uint32_t b)
-{
-	int32_t out;
-	asm volatile("smulwb %0, %1, %2" : "=r" (out) : "r" (a), "r" (b));
-	return out;
-}
-
-// computes ((a[31:0] * b[31:16]) >> 16)
-static inline int32_t signed_multiply_32x16t(int32_t a, uint32_t b) __attribute__((always_inline));
-static inline int32_t signed_multiply_32x16t(int32_t a, uint32_t b)
-{
-	int32_t out;
-	asm volatile("smulwt %0, %1, %2" : "=r" (out) : "r" (a), "r" (b));
-	return out;
-}
-
-// computes (((int64_t)a[31:0] * (int64_t)b[31:0]) >> 32)
-static inline int32_t multiply_32x32_rshift32(int32_t a, int32_t b) __attribute__((always_inline));
-static inline int32_t multiply_32x32_rshift32(int32_t a, int32_t b)
-{
-	int32_t out;
-	asm volatile("smmul %0, %1, %2" : "=r" (out) : "r" (a), "r" (b));
-	return out;
-}
-
-// computes (((int64_t)a[31:0] * (int64_t)b[31:0] + 0x8000000) >> 32)
-static inline int32_t multiply_32x32_rshift32_rounded(int32_t a, int32_t b) __attribute__((always_inline));
-static inline int32_t multiply_32x32_rshift32_rounded(int32_t a, int32_t b)
-{
-	int32_t out;
-	asm volatile("smmulr %0, %1, %2" : "=r" (out) : "r" (a), "r" (b));
-	return out;
-}
-
-// computes sum + (((int64_t)a[31:0] * (int64_t)b[31:0] + 0x8000000) >> 32)
-static inline int32_t multiply_accumulate_32x32_rshift32_rounded(int32_t sum, int32_t a, int32_t b) __attribute__((always_inline));
-static inline int32_t multiply_accumulate_32x32_rshift32_rounded(int32_t sum, int32_t a, int32_t b)
-{
-	int32_t out;
-	asm volatile("smmlar %0, %2, %3, %1" : "=r" (out) : "r" (sum), "r" (a), "r" (b));
-	return out;
-}
-
-// computes sum - (((int64_t)a[31:0] * (int64_t)b[31:0] + 0x8000000) >> 32)
-static inline int32_t multiply_subtract_32x32_rshift32_rounded(int32_t sum, int32_t a, int32_t b) __attribute__((always_inline));
-static inline int32_t multiply_subtract_32x32_rshift32_rounded(int32_t sum, int32_t a, int32_t b)
-{
-	int32_t out;
-	asm volatile("smmlsr %0, %2, %3, %1" : "=r" (out) : "r" (sum), "r" (a), "r" (b));
-	return out;
-}
-
-// computes ((a[15:0] << 16) | b[15:0])
-static inline uint32_t pack_16x16(int32_t a, int32_t b) __attribute__((always_inline));
-static inline uint32_t pack_16x16(int32_t a, int32_t b)
-{
-	int32_t out;
-	asm volatile("pkhbt %0, %1, %2, lsl #16" : "=r" (out) : "r" (b), "r" (a));
-	return out;
-}
-
-// computes (((a[31:16] + b[31:16]) << 16) | (a[15:0 + b[15:0]))
-static inline uint32_t signed_add_16_and_16(uint32_t a, uint32_t b) __attribute__((always_inline));
-static inline uint32_t signed_add_16_and_16(uint32_t a, uint32_t b)
-{
-	int32_t out;
-	asm volatile("qadd16 %0, %1, %2" : "=r" (out) : "r" (a), "r" (b));
-	return out;
-}
-
-// computes (sum + ((a[31:0] * b[15:0]) >> 16))
-static inline int32_t signed_multiply_accumulate_32x16b(int32_t sum, int32_t a, uint32_t b)
-{
-	int32_t out;
-	asm volatile("smlawb %0, %2, %3, %1" : "=r" (out) : "r" (sum), "r" (a), "r" (b));
-	return out;
-}
-
-// computes (sum + ((a[31:0] * b[31:16]) >> 16))
-static inline int32_t signed_multiply_accumulate_32x16t(int32_t sum, int32_t a, uint32_t b)
-{
-	int32_t out;
-	asm volatile("smlawt %0, %2, %3, %1" : "=r" (out) : "r" (sum), "r" (a), "r" (b));
-	return out;
-}
-
-// computes logical and, forces compiler to allocate register and use single cycle instruction
-static inline uint32_t logical_and(uint32_t a, uint32_t b)
-{
-	asm volatile("and %0, %1" : "+r" (a) : "r" (b));
-	return a;
-}
-
-
 
 
 void applyGain(int16_t *data, int32_t mult)
