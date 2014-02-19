62698957fbde3f1b99ea7b65b3c81c9dbdb10e8f
Pete (El Supremo) <peteros@shaw.ca>
2014-02-18 19:08:40 -0600

    Add an audio sweep. User defined start and end frequencies and length of time of sweep.

--- Audio.h
+++ Audio.h
@@ -666,6 +666,30 @@ private:
 };
 
 
+/******************************************************************/
 
+//                A u d i o T o n e S w e e p
+// Written by Pete (El Supremo) Feb 2014
 
+class AudioToneSweep : public AudioStream
+{
+public:
+  AudioToneSweep(void) : 
+  AudioStream(0,NULL), sweep_busy(0)
+  { }
+
+  boolean begin(short t_amp,int t_lo,int t_hi,float t_time);
+  virtual void update(void);
+  unsigned char busy(void);
+
+private:
+  short tone_amp;
+  int tone_lo;
+  int tone_hi;
+  uint64_t tone_freq;
+  uint64_t tone_phase;
+  uint64_t tone_incr;
+  int tone_sign;
+  unsigned char sweep_busy;
+};
 
