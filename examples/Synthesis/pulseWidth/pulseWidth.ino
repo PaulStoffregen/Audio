07919f1597d9b552ba3fa0e256e7213194ff9b86
Nantonos <nantonos@epona.net>
2014-09-12 05:46:13 +0200

    width -> pulseWidth
    Change width to pulseWidth, update the example, keywords.txt and
    gui/list.htm

--- examples/Synthesis/pulseWidth/pulseWidth.ino
+++ examples/Synthesis/pulseWidth/pulseWidth.ino
@@ -1,4 +1,4 @@
-// demonstrate pulse with slow changes in width
+﻿// demonstrate pulse with slow changes in pulse width
 
 #include <Audio.h>
 #include <Wire.h>
@@ -24,7 +24,7 @@ void setup(void)
   audioShield.enable();
   audioShield.volume(0.45);
 
-  waveform1.width(0.5);
+  waveform1.pulseWidth(0.5);
   waveform1.begin(0.4, 220, WAVEFORM_PULSE);
 
   envelope1.attack(50);
@@ -38,7 +38,7 @@ void loop() {
   float w;
   for (uint32_t i =1; i<20; i++) {
     w = i / 20.0;
-    waveform1.width(w);
+    waveform1.pulseWidth(w);
     envelope1.noteOn();
     delay(800);
     envelope1.noteOff();
