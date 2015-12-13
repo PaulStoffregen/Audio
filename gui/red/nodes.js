c3d31d54fd151976de6423e30a1b77f9ef0a535b
Manfred Müller-Späth <fms1961@gmail.com>
2015-12-13 18:40:38 +0100

    Small fixes: the update of the "info" parameter in the URL when selecting a node was switched off, this is reactivated (nodes.js) Correction of a typo in the HTML page source in index.html

--- gui/red/nodes.js
+++ gui/red/nodes.js
@@ -90,8 +90,8 @@ RED.nodes = (function() {
 		return node_defs[type];
 	}
 	function selectNode(name) {
-	//	window.history.pushState(null, null, window.location.protocol + "//"
-	//		+ window.location.host + window.location.pathname + '?info=' + name);
+		windowq.history.pushState(null, null, window.location.protocol + "//"
+			+ window.location.host + window.location.pathname + '?info=' + name);
 	}
 	function addNode(n) {
 		if (n._def.category == "config") {
