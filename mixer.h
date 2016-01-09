9a9014363b377e0af9c1560110ed533c2a6db5e0
PaulStoffregen <paul@pjrc.com>
2016-01-09 03:26:40 -0800

    Allow negative/inverting mixer gain fixes #115

--- mixer.h
+++ mixer.h
@@ -40,7 +40,7 @@ public:
 	void gain(unsigned int channel, float gain) {
 		if (channel >= 4) return;
 		if (gain > 32767.0f) gain = 32767.0f;
-		else if (gain < 0.0f) gain = 0.0f;
+		else if (gain < -32767.0f) gain = -32767.0f;
 		multiplier[channel] = gain * 65536.0f; // TODO: proper roundoff?
 	}
 private:
@@ -56,7 +56,7 @@ public:
 	void gain(unsigned int channel, float gain) {
 		if (channel >= 4) return;
 		if (gain > 127.0f) gain = 127.0f;
-		else if (gain < 0.0f) gain = 0.0f;
+		else if (gain < -127.0f) gain = -127.0f;
 		multiplier[channel] = gain * 256.0f; // TODO: proper roundoff?
 	}
 private:
