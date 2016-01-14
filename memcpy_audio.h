b76dd6621eb078f227742e918b3a88a49f8b9207
Frank Bösing <f.boesing@gmx.de>
2016-01-14 19:28:03 +0100

    Add assembler-code & use it in output_i2s

---
+++ memcpy_audio.h
@@ -0,0 +1,44 @@
+/* Teensyduino Audio Memcpy
+ * Copyright (c) 2016 Frank Bösing
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * 1. The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * 2. If the Software is incorporated into a build system that allows
+ * selection among a list of target devices, then similar target
+ * devices manufactured by PJRC.COM must be included in the list of
+ * target devices and selectable in the same manner.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef memcpy_audio_h_
+#define memcpy_audio_h_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void memcpy_tointerleaveLR(int16_t *dst, int16_t *srcL, int16_t *srcR);
+void memcpy_tointerleaveL(int16_t *dst, int16_t *srcL);
+void memcpy_tointerleaveR(int16_t *dst, int16_t *srcR);
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
