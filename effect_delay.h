b29d47455f187a53381122d458f204016e3139e0
PaulStoffregen <paul@pjrc.com>
2017-03-27 17:20:44 -0700

    Increase maximum delay and scale with block size

--- effect_delay.h
+++ effect_delay.h
@@ -30,7 +30,19 @@
 #include "AudioStream.h"
 #include "utility/dspinst.h"
 
-#define DELAY_QUEUE_SIZE  117
+#if defined(__MK66FX1M0__)
+  // 2.41 second maximum on Teensy 3.6
+  #define DELAY_QUEUE_SIZE  (106496 / AUDIO_BLOCK_SAMPLES)
+#elif defined(__MK64FX512__)
+  // 1.67 second maximum on Teensy 3.5
+  #define DELAY_QUEUE_SIZE  (73728 / AUDIO_BLOCK_SAMPLES)
+#elif defined(__MK20DX256__)
+  // 0.45 second maximum on Teensy 3.1 & 3.2
+  #define DELAY_QUEUE_SIZE  (19826 / AUDIO_BLOCK_SAMPLES)
+#else
+  // 0.14 second maximum on Teensy 3.0
+  #define DELAY_QUEUE_SIZE  (6144 / AUDIO_BLOCK_SAMPLES)
+#endif
 
 class AudioEffectDelay : public AudioStream
 {
