98a6be451120700e601ca4e4fa83907e46b6a1bf
PaulStoffregen <paul@pjrc.com>
2017-12-09 02:12:23 -0800

    Add ADAT 8 channel TOSLINK output by Ernstjan Freriks https://forum.pjrc.com/threads/28639-S-pdif?p=159530&viewfull=1#post159530

--- Audio.h
+++ Audio.h
@@ -98,6 +98,7 @@
 #include "output_spdif.h"
 #include "output_pt8211.h"
 #include "output_tdm.h"
+#include "output_adat.h"
 #include "play_memory.h"
 #include "play_queue.h"
 #include "play_sd_raw.h"
