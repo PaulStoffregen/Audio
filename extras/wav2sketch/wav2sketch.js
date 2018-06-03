1bec7b51c3373671a19da77c5abf94d42fa2f90d
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2018-06-03 16:12:55 +0100

    loading files and playing them using web audio api

--- extras/wav2sketch/wav2sketch.js
+++ extras/wav2sketch/wav2sketch.js
@@ -1,1 +1,23 @@
-alert("Hello!");
+var audioFileChooser = document.getElementById('audioFileChooser');
+
+audioFileChooser.addEventListener('change', readFile);
+
+function readFile() {
+  // TODO: deal with multiple files
+  var fileReader = new FileReader();
+  fileReader.readAsArrayBuffer(audioFileChooser.files[0]);
+  fileReader.addEventListener('load', function(ev) {
+    processFile(ev.target.result);
+  });
+}
+
+function processFile(file) {
+  var context = new window.AudioContext();
+	context.decodeAudioData(file, function(buffer) {
+  	var source = context.createBufferSource();
+  	source.buffer = buffer;
+  	source.loop = false;
+  	source.connect(context.destination);
+  	source.start(0); 
+	});
+}
