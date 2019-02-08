246d051b01b4bed6849cb9be93f36aaa4f74af55
Frank Bösing <f.boesing@gmx.de>
2019-02-09 00:03:28 +0100

    add output_mqs

--- Audio.h
+++ Audio.h
@@ -101,6 +101,7 @@
 #include "output_i2s.h"
 #include "output_i2s2.h"
 #include "output_i2s_quad.h"
+#include "output_mqs.h"
 #include "output_pwm.h"
 #include "output_spdif.h"
 #include "output_pt8211.h"
