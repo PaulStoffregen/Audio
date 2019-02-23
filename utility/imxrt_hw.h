c99b7b720469a0cdcf6480490cb9a081c24434d2
Frank Bösing <f.boesing@gmx.de>
2019-02-23 17:23:14 +0100

    initial  cache-support

--- utility/imxrt_hw.h
+++ utility/imxrt_hw.h
@@ -32,10 +32,17 @@
 #ifndef imxr_hw_h_
 #define imxr_hw_h_
 
+#define IMXRT_CACHE_ENABLED 2 // 0=disabled, 1=WT, 2= WB
+
 #include <Arduino.h>
 #include <imxrt.h>
 
 void set_audioClock(int nfact, int32_t nmult, uint32_t ndiv); // sets PLL4
 
 #endif
+
+#else
+//No IMXRT
+#define IMXRT_CACHE_ENABLED 0
 #endif
+	
