a2bc7ab6fc473ce55577baf26c8a20d3c1c86e9a
Nic Newdigate <nicnewdigate@gmail.com>
2019-03-03 16:14:33 +0000

    Create build-sketches.sh

---
+++ build-sketches.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# define colors
+GRAY='\033[1;30m'; RED='\033[0;31m'; LRED='\033[1;31m'; GREEN='\033[0;32m'; LGREEN='\033[1;32m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; LBLUE='\033[1;34m'; PURPLE='\033[0;35m'; LPURPLE='\033[1;35m'; CYAN='\033[0;36m'; LCYAN='\033[1;36m'; LGRAY='\033[0;37m'; WHITE='\033[1;37m';
+examples=($(find $HOME/build -name "*.pde" -o -name "*.ino"))
+for example in "${examples[@]}"; do
+  echo -n $example:
+  local platform_stdout=$($HOME/arduino_ide/arduino-$ARDUINO_IDE_VERSION/arduino --verify --board "teensy:avr:teensy36:usb=serial,speed=180,opt=o2std,keys=en-us" $example 2>&1)
+  # grab the exit status of the arduino board change
+  local platform_switch=$?
+  # notify if the platform switch failed
+  if [ $platform_switch -ne 0 ]; then
+    # heavy X
+    echo -e """$RED""\xe2\x9c\x96"
+    
+    echo $platform_stdout
+    exit_code=1
+  else
+    # heavy checkmark
+    echo -e """$GREEN""\xe2\x9c\x93"
+  fi
+done;
