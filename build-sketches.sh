96cb712b48b021d6ae4a309c1208d6638fd034a6
Nic Newdigate <nicnewdigate@gmail.com>
2019-03-03 20:36:32 +0000

    excl Tutorials

--- build-sketches.sh
+++ build-sketches.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # define colors
 GRAY='\033[1;30m'; RED='\033[0;31m'; LRED='\033[1;31m'; GREEN='\033[0;32m'; LGREEN='\033[1;32m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; LBLUE='\033[1;34m'; PURPLE='\033[0;35m'; LPURPLE='\033[1;35m'; CYAN='\033[0;36m'; LCYAN='\033[1;36m'; LGRAY='\033[0;37m'; WHITE='\033[1;37m';
-examples=($(find $HOME/build -name "*.pde" -o -name "*.ino"))
+examples=($(find $HOME/build -name "*.pde" -o -name "*.ino" -not -path "*/Tutorial/*"))
 for example in "${examples[@]}"; do
   echo -n $example:
   $HOME/arduino_ide/arduino-$ARDUINO_IDE_VERSION/arduino --verify --board "teensy:avr:teensy36:usb=serial,speed=180,opt=o2std,keys=en-us" $example 2> error.txt > output.txt
