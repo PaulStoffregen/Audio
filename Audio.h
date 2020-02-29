16d4c562a93351156cc02cbb1baf5685c7141ce3
PaulStoffregen <paul@pjrc.com>
2020-02-28 16:15:15 -0800

    8 channel I2S output for Teensy 4.0

--- Audio.h
+++ Audio.h
@@ -103,6 +103,7 @@
 #include "output_i2s2.h"
 #include "output_i2s_quad.h"
 #include "output_i2s_hex.h"
+#include "output_i2s_oct.h"
 #include "output_mqs.h"
 #include "output_pwm.h"
 #include "output_spdif.h"
