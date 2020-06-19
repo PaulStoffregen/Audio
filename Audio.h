a625bb7ab81bec2b44b2d4a24b2000567a8bdc76
PaulStoffregen <paul@pjrc.com>
2020-06-18 17:09:30 -0700

    Add rectifier effect

--- Audio.h
+++ Audio.h
@@ -86,6 +86,7 @@
 #include "effect_waveshaper.h"
 #include "effect_granular.h"
 #include "effect_combine.h"
+#include "effect_rectifier.h"
 #include "filter_biquad.h"
 #include "filter_fir.h"
 #include "filter_variable.h"
