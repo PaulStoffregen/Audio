d012b952e97f687b5276940d5ef059db72d146ce
PaulStoffregen <paul@pjrc.com>
2020-06-26 22:42:43 -0700

    Fix PT8211 on Teensy 4.x

--- output_pt8211_2.cpp
+++ output_pt8211_2.cpp
@@ -80,7 +80,7 @@ void AudioOutputPT8211_2::begin(void)
 
 void AudioOutputPT8211_2::isr(void)
 {
-	int16_t *dest;
+	int16_t *dest, *dest_copy;
 	audio_block_t *blockL, *blockR;
 	uint32_t saddr, offsetL, offsetR;
 
@@ -100,6 +100,7 @@ void AudioOutputPT8211_2::isr(void)
 		// so we must fill the first half
 		dest = (int16_t *)i2s_tx_buffer;
 	}
+	dest_copy = dest;
 
 	blockL = AudioOutputPT8211_2::block_left_1st;
 	blockR = AudioOutputPT8211_2::block_right_1st;
@@ -313,6 +314,8 @@ void AudioOutputPT8211_2::isr(void)
 		#endif
 		return;
 	}
+
+	arm_dcache_flush_delete(dest_copy, sizeof(i2s_tx_buffer) / 2);
 
 	if (offsetL < AUDIO_BLOCK_SAMPLES) {
 		AudioOutputPT8211_2::block_left_offset = offsetL;
