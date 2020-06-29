4db1dd3d5b58b45a6b2d15f00c9131d53268918c
manicken <65423412+manicken@users.noreply.github.com>
2020-06-29 21:19:16 +0200

    Add template based mixer
    This template based mixer called AudioMixerX can take any number of inputs 
    by just use
    AudioMixerX<numOfInputs> mixer;

--- mixer.h
+++ mixer.h
@@ -66,6 +66,142 @@ private:
 #endif
 };
 
+template <size_t N>
+class AudioMixerX : public AudioStream
+{
+#if defined(__ARM_ARCH_7EM__)
+#define MULTI_UNITYGAIN 65536
+public:
+	AudioMixer4(void) : AudioStream(N, inputQueueArray) {
+		for (int i=0; i<N; i++) multiplier[i] = MULTI_UNITYGAIN;
+	}
+	void update(void)
+	{
+		audio_block_t *in, *out=NULL;
+		unsigned int channel;
+
+		for (channel=0; channel < N; channel++) {
+			if (!out) {
+				out = receiveWritable(channel);
+				if (out) {
+					int32_t mult = multiplier[channel];
+					if (mult != MULTI_UNITYGAIN) applyGain(out->data, mult);
+				}
+			} else {
+				in = receiveReadOnly(channel);
+				if (in) {
+					applyGainThenAdd(out->data, in->data, multiplier[channel]);
+					release(in);
+				}
+			}
+		}
+		if (out) {
+			transmit(out);
+			release(out);
+		}
+	}
+	void gain(unsigned int channel, float gain) {
+		if (channel >= N) return;
+		if (gain > 32767.0f) gain = 32767.0f;
+		else if (gain < -32767.0f) gain = -32767.0f;
+		multiplier[channel] = gain * 65536.0f; // TODO: proper roundoff?
+	}
+	
+
+	static void applyGain(int16_t *data, int32_t mult)
+	{
+		uint32_t *p = (uint32_t *)data;
+		const uint32_t *end = (uint32_t *)(data + AUDIO_BLOCK_SAMPLES);
+
+		do {
+			uint32_t tmp32 = *p; // read 2 samples from *data
+			int32_t val1 = signed_multiply_32x16b(mult, tmp32);
+			int32_t val2 = signed_multiply_32x16t(mult, tmp32);
+			val1 = signed_saturate_rshift(val1, 16, 0);
+			val2 = signed_saturate_rshift(val2, 16, 0);
+			*p++ = pack_16b_16b(val2, val1);
+		} while (p < end);
+	}
+
+	static void applyGainThenAdd(int16_t *data, const int16_t *in, int32_t mult)
+	{
+		uint32_t *dst = (uint32_t *)data;
+		const uint32_t *src = (uint32_t *)in;
+		const uint32_t *end = (uint32_t *)(data + AUDIO_BLOCK_SAMPLES);
+
+		if (mult == MULTI_UNITYGAIN) {
+			do {
+				uint32_t tmp32 = *dst;
+				*dst++ = signed_add_16_and_16(tmp32, *src++);
+				tmp32 = *dst;
+				*dst++ = signed_add_16_and_16(tmp32, *src++);
+			} while (dst < end);
+		} else {
+			do {
+				uint32_t tmp32 = *src++; // read 2 samples from *data
+				int32_t val1 = signed_multiply_32x16b(mult, tmp32);
+				int32_t val2 = signed_multiply_32x16t(mult, tmp32);
+				val1 = signed_saturate_rshift(val1, 16, 0);
+				val2 = signed_saturate_rshift(val2, 16, 0);
+				tmp32 = pack_16b_16b(val2, val1);
+				uint32_t tmp32b = *dst;
+				*dst++ = signed_add_16_and_16(tmp32, tmp32b);
+			} while (dst < end);
+		}
+	}
+private:
+	int32_t multiplier[N];
+	audio_block_t *inputQueueArray[N];
+
+#elif defined(KINETISL)
+#define MULTI_UNITYGAIN 256
+public:
+	AudioMixer4(void) : AudioStream(N, inputQueueArray) {
+		for (int i=0; i<N; i++) multiplier[i] = MULTI_UNITYGAIN;
+	}
+	virtual void update(void);
+	void gain(unsigned int channel, float gain) {
+		if (channel >= N) return;
+		if (gain > 127.0f) gain = 127.0f;
+		else if (gain < -127.0f) gain = -127.0f;
+		multiplier[channel] = gain * 256.0f; // TODO: proper roundoff?
+	}
+	
+
+	static void applyGain(int16_t *data, int32_t mult)
+	{
+		const int16_t *end = data + AUDIO_BLOCK_SAMPLES;
+
+		do {
+			int32_t val = *data * mult;
+			*data++ = signed_saturate_rshift(val, 16, 0);
+		} while (data < end);
+	}
+
+	static void applyGainThenAdd(int16_t *dst, const int16_t *src, int32_t mult)
+	{
+		const int16_t *end = dst + AUDIO_BLOCK_SAMPLES;
+
+		if (mult == MULTI_UNITYGAIN) {
+			do {
+				int32_t val = *dst + *src++;
+				*dst++ = signed_saturate_rshift(val, 16, 0);
+			} while (dst < end);
+		} else {
+			do {
+				int32_t val = *dst + ((*src++ * mult) >> 8); // overflow possible??
+				*dst++ = signed_saturate_rshift(val, 16, 0);
+			} while (dst < end);
+		}
+	}
+
+#endif
+private:
+	int16_t multiplier[N];
+	audio_block_t *inputQueueArray[N];
+#endif
+};
+
 class AudioAmplifier : public AudioStream
 {
 public:
