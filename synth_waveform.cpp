abeaf945205ead7a0284822f4804b47c285d2bc6
user <user@users-MacBook-Pro.local>
2020-08-01 03:54:02 +0100

    Alternative fix without signed overflow issues

--- synth_waveform.cpp
+++ synth_waveform.cpp
@@ -243,7 +243,7 @@ void AudioSynthWaveformModulated::update(void)
 		bp = moddata->data;
 		for (i=0; i < AUDIO_BLOCK_SAMPLES; i++) {
 			// more than +/- 180 deg shift by 32 bit overflow of "n"
-			uint32_t n = (*bp++) * modulation_factor;
+		        uint32_t n = ((uint32_t)(*bp++)) * modulation_factor;
 			phasedata[i] = ph + n;
 			ph += inc;
 		}
