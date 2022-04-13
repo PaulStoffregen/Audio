c8dff3aae708ecb5d6a25bd27bf9f5a4486d60de
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2022-04-13 11:45:02 +0100

    Fix issues with cache flush and non-silent startup
    SPDIF3 was flushing cache before writing the memory, so 8 stale samples could be output
    No SPDIF was zeroing the buffer, so a brief garbage burst could happen at startup
    ADAT was not flushing the cache (this is untested)

--- output_spdif3.cpp
+++ output_spdif3.cpp
@@ -61,6 +61,7 @@ FLASHMEM
 void AudioOutputSPDIF3::begin(void)
 {
 
+	memset(SPDIF_tx_buffer,0,sizeof SPDIF_tx_buffer); // ensure we start with silence
 	dma.begin(true); // Allocate the DMA channel first
 
 	block_left_1st = nullptr;
@@ -127,22 +128,26 @@ void AudioOutputSPDIF3::isr(void)
 	src_right = (const int16_t *)(block_right->data);
 
 	do {
+		*dest++ = (*src_left++) << 8;
+		*dest++ = (*src_right++) << 8;
+
+		*dest++ = (*src_left++) << 8;
+		*dest++ = (*src_right++) << 8;
+
+		*dest++ = (*src_left++) << 8;
+		*dest++ = (*src_right++) << 8;
+
+		*dest++ = (*src_left++) << 8;
+		*dest++ = (*src_right++) << 8;
+	
+		// Flush the cache every 32 bytes as we go along, as suggested in forum post 
+		// https://forum.pjrc.com/threads/54711-Teensy-4-0-First-Beta-Test?p=194676&viewfull=1#post194676
+		// Experience suggests it works better to flush 
+		// after the copy, rather than before...
 		#if IMXRT_CACHE_ENABLED >= 2
-		SCB_CACHE_DCCIMVAC = (uintptr_t) dest;
+		SCB_CACHE_DCCIMVAC = (uintptr_t) &(dest[-8]);
 		asm volatile("dsb");
 		#endif
-		
-		*dest++ = (*src_left++) << 8;
-		*dest++ = (*src_right++) << 8;
-
-		*dest++ = (*src_left++) << 8;
-		*dest++ = (*src_right++) << 8;
-
-		*dest++ = (*src_left++) << 8;
-		*dest++ = (*src_right++) << 8;
-
-		*dest++ = (*src_left++) << 8;
-		*dest++ = (*src_right++) << 8;
 
 	} while (dest < end);
 
