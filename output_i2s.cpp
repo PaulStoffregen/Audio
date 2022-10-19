7e1d12853f738efdb3efbecb4a2e6d4bd47be668
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2022-10-19 09:26:32 +0100

    Various tries at getting S/PDIF MCLK master
    ...all unsuccessful

--- output_i2s.cpp
+++ output_i2s.cpp
@@ -403,7 +403,7 @@ void AudioOutputI2S::config_i2s(bool only_bclk /* = false */, bool SPDIF_sync /*
 	// if not setting up new SPDIF sync, and
 	// either transmitter or receiver is enabled, do nothing
 	if (!SPDIF_sync &&
-	    ((I2S1_TCSR & I2S_TCSR_TE) != 0 || (I2S1_RCSR & (I2S_RCSR_RE | I2S_RCSR_BCE) == (I2S_RCSR_RE | I2S_RCSR_BCE)))
+	    ((I2S1_TCSR & I2S_TCSR_TE) != 0 || ((I2S1_RCSR & (I2S_RCSR_RE | I2S_RCSR_BCE)) == (I2S_RCSR_RE | I2S_RCSR_BCE)))
 		)
 	{
 	  if (!only_bclk) // if previous transmitter/receiver only activated BCLK, activate the other clock pins now
@@ -415,10 +415,10 @@ void AudioOutputI2S::config_i2s(bool only_bclk /* = false */, bool SPDIF_sync /*
 	}
 
 	//PLL:
-	int fs = AUDIO_SAMPLE_RATE_EXACT;
+	double fs = AUDIO_SAMPLE_RATE_EXACT;
 	// PLL between 27*24 = 648MHz und 54*24=1296MHz
 	int n1 = 4; //SAI prescaler 4 => (n1*n2) = multiple of 4
-	int n2 = 1 + (24000000 * 27) / (fs * 256 * n1);
+	int n2 = 1 + (24000000 * 27) / ((int) fs * 256 * n1);
 
 	double C = ((double)fs * 256 * n1 * n2) / 24000000;
 	int c0 = C;
@@ -459,12 +459,31 @@ void AudioOutputI2S::config_i2s(bool only_bclk /* = false */, bool SPDIF_sync /*
 	if (SPDIF_sync)
 		SPDIF_is_master = true;
 
+	// I2S1 / SAI1 registers
+	uint32_t oldTE = I2S1_TCSR & I2S_TCSR_TE,
+			 oldRE = I2S1_RCSR & I2S_RCSR_RE;
+	I2S1_TCSR &= ~I2S_TCSR_TE; // can't touch TCR2 if TE is set
+	I2S1_RCSR &= ~I2S_RCSR_RE; // can't touch RCR2 if RE is set
+	
 	I2S1_TMR = 0;
 	//I2S1_TCSR = (1<<25); //Reset
 	I2S1_TCR1 = I2S_TCR1_RFW(1);
 	
 	if (SPDIF_is_master)
+	{
 		div_and_mclk = I2S_TCR2_DIV((0)) | I2S_TCR2_MSEL(3); // MCLK[3] / 2
+//*/		
+		IOMUXC_GPR_GPR1 = (IOMUXC_GPR_GPR1
+			& ~(IOMUXC_GPR_GPR1_SAI1_MCLK1_SEL_MASK))
+			|   IOMUXC_GPR_GPR1_SAI1_MCLK1_SEL(1)
+			;
+// nope			& ~(IOMUXC_GPR_GPR1_SAI1_MCLK_DIR));
+			//*/
+//*/
+		CCM_ANALOG_PLL_AUDIO &= ~(CCM_ANALOG_PLL_AUDIO_ENABLE);
+//		CCM_ANALOG_PLL_AUDIO |=  (CCM_ANALOG_PLL_AUDIO_POWERDOWN);
+//*/
+	}
 	else
 		div_and_mclk = I2S_TCR2_DIV((1)) | I2S_TCR2_MSEL(1); // MCLK[1] / 4
 		
@@ -490,6 +509,9 @@ void AudioOutputI2S::config_i2s(bool only_bclk /* = false */, bool SPDIF_sync /*
 	I2S1_RCR4 = I2S_RCR4_FRSZ((2-1)) | I2S_RCR4_SYWD((32-1)) | I2S_RCR4_MF
 		    | I2S_RCR4_FSE | I2S_RCR4_FSP | I2S_RCR4_FSD;
 	I2S1_RCR5 = I2S_RCR5_WNW((32-1)) | I2S_RCR5_W0W((32-1)) | I2S_RCR5_FBT((32-1));
+	
+	I2S1_TCSR |= oldTE; // restore old Transmit Enable...
+	I2S1_RCSR |= oldRE; // ...and Receive Enable
 
 #endif
 }
