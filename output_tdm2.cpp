2c263b0bb96e9c121d524f2cfcc6ca314dba9ff6
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2024-02-17 16:14:19 +0000

    Zero out the DMAMEM buffers
    ...like AudioOutputTDM does

--- output_tdm2.cpp
+++ output_tdm2.cpp
@@ -49,7 +49,11 @@ void AudioOutputTDM2::begin(void)
 	for (int i=0; i < 16; i++) {
 		block_input[i] = nullptr;
 	}
-
+	
+	// DMAMEM doesn't get zeroed, have to do it ourselves:
+	memset(zeros, 0, sizeof zeros);
+	memset(tdm_tx_buffer, 0, sizeof tdm_tx_buffer);
+	
 	// TODO: should we set & clear the I2S_TCSR_SR bit here?
 	config_tdm();
 
